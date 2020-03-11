5f7986e3af237690fd3442960beff2d953fc89e9
fpired18 <fpired@wanadoo.fr>
2020-03-11 16:23:39 +0100

    mise a jour du projet

---
+++ I Am Rich/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I Am Rich
+//
+//  Created by etudiant-13 on 19/11/2018.
+//  Copyright © 2018 CCI-indre. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
